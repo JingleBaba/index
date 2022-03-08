28b427209efb5706a3bce2dfc3be3c7cc7f3fcfd
jingle-baba <umesh@anattadesign.com>
2022-03-08 08:06:58 +0530

    hover color fix

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="sidebar col-span-2 min-h-screen pt-12 bg-gray-50">
-      <h2 class="text-xl font-medium text-slate-900 dark:text-slate-200 p-6 bg-white" data-id="introduction">Introduction</h2>
-       <h2 class="text-xl font-medium text-slate-900 dark:text-slate-200 p-6" data-id="introduction">Custom Types</h2>
+    <div class="sidebar col-span-2 min-h-screen pt-12 bg-gray-100">
+      <h2 class="text-xl font-medium text-slate-900 dark:text-slate-200 p-6 bg-white mb-2" data-id="introduction">Introduction</h2>
+      <h2 class="text-xl font-medium text-slate-900 dark:text-slate-200 p-6 hover:bg-white cursor-pointer" data-id="introduction">Custom Types</h2>
     </div>
 </template>
 
