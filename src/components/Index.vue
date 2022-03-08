bd7b7d73baecdb3e7be0b2c54cdcbecbacae054f
jingle-baba <umesh@anattadesign.com>
2022-03-08 07:45:08 +0530

    add dist

---
+++ src/components/Index.vue
@@ -0,0 +1,21 @@
+
+<script>
+import Sidebar from "./Sidebar/Sidebar.vue";
+import Content from "./Content/Content.vue";
+export default {
+  setup () {
+  },
+  components: {
+    Content,
+    Sidebar,
+  },
+};
+</script>
+
+
+<template>
+  <div className="grid grid-cols-12 gap-x-10 font-poppins">
+    <Sidebar />
+    <Content />
+  </div>
+</template>
---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="sidebar col-span-2 min-h-screen pt-12 bg-gray-50">
+      <h2 class="text-xl font-medium text-slate-900 dark:text-slate-200 p-6 bg-white" data-id="introduction">Introduction</h2>
+       <h2 class="text-xl font-medium text-slate-900 dark:text-slate-200 p-6" data-id="introduction">Custom Types</h2>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
